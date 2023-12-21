e9328f01f42440b516bc590d0c7f0b3ae4242457
Pisukeindo <145236948+Pisukeindo@users.noreply.github.com>
2023-12-22 01:18:52 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import login
-from Manajemen import pertambahan_aset,suplier,karyawan
-from Harian import bahan_baku_harian,quality_control,penjualan_harian,pengeluaran_harian,produksi_harian
-from Laporan import output
+#from Manajemen import pertambahan_aset,suplier,karyawan
+#from Harian import bahan_baku_harian,quality_control,penjualan_harian,pengeluaran_harian,produksi_harian
+#from Laporan import output
 # Inisialisasi status login
 if "username" not in st.session_state:
     st.session_state.username = None
@@ -24,19 +24,19 @@ else:
 
         if selected_page == "Quality Control Harian":
             st.title("Quality Control")
-            quality_control.quality_control()
+ #           quality_control.quality_control()
         elif selected_page == "Penjualan Harian":
             st.title("Penjualan Harian")
-            penjualan_harian.penjualan_harian()
+  #          penjualan_harian.penjualan_harian()
         elif selected_page == "Bahan Baku Harian":
             st.title("Bahan Baku Harian")
-            bahan_baku_harian.bahan_baku_harian()
+   #         bahan_baku_harian.bahan_baku_harian()
         elif selected_page == "Pengeluaran Harian":
             st.title("Pengeluaran Harian")
-            pengeluaran_harian.pengeluaran_harian()
+    #        pengeluaran_harian.pengeluaran_harian()
         elif selected_page == "Produksi Harian":
             st.title("Produksi Harian")
-            produksi_harian.produksi_harian()
+     #       produksi_harian.produksi_harian()
 
     elif selected_menu == "Data Manajemen":
         selected_page = st.sidebar.radio(
@@ -46,11 +46,11 @@ else:
 
         if selected_page == "Suplier":
             
-            suplier.suplier()
+      #      suplier.suplier()
         elif selected_page == "Pertambahan Aset":
-            pertambahan_aset.pertambahan_aset()
+       #     pertambahan_aset.pertambahan_aset()
         elif selected_page == "Karyawan":
-            karyawan.karyawan()
+        #    karyawan.karyawan()
         
 
     elif selected_menu == "Laporan Harian":
@@ -60,17 +60,17 @@ else:
         )
 
         if selected_laporan == "Bahan Baku Harian":
-            output.laporan("bahan_baku_harian")
+         #   output.laporan("bahan_baku_harian")
         elif selected_laporan == "Pengeluaran Harian":
-            output.laporan("pengeluaran_harian")
+          #  output.laporan("pengeluaran_harian")
         elif selected_laporan == "Produksi Harian":
-            output.laporan("produksi_harian")
+           # output.laporan("produksi_harian")
         elif selected_laporan == "Penjualan Harian":
-            output.laporan("penjualan_harian")
+            #output.laporan("penjualan_harian")
         elif selected_laporan == "Stok Bahan Baku":
-            output.laporan("stok_bahan_baku")
+          #  output.laporan("stok_bahan_baku")
         elif selected_laporan == "Quality Control":
-            output.laporan("qc")
+           # output.laporan("qc")
 
 
     elif selected_menu == "Laporan Manajemen":
@@ -80,9 +80,9 @@ else:
         )
 
         if selected_laporan == "Pertambahan Aser":
-            output.laporan("Pertambahan Aset")
+  #          output.laporan("Pertambahan Aset")
         elif selected_laporan == "Suplier":
-            output.laporan("suplier")
+ #           output.laporan("suplier")
         elif selected_laporan == "Karyawan":
-            output.laporan("karyawan")
+#            output.laporan("karyawan")
      
